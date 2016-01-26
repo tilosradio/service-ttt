e520f20cee85535488139bc413297538982cd577
Elek, Márton <elek@users.noreply.github.com>
2016-01-26 20:51:38 +0100

    new: eureka and config client

--- src/main/java/hu/tilos/radio/backend/ttt/TTTStarter.java
+++ src/main/java/hu/tilos/radio/backend/ttt/TTTStarter.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 @RestController
 @ComponentScan
-//@EnableEurekaClient
+@EnableEurekaClient
 public class TTTStarter {
 
     @Autowired
